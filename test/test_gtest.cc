ff2193f7a070e901222c3584740005a55ccdf0e2
szdytom <szdytom@163.com>
2020-08-18 21:32:07 +0800

    add main to tests

--- test/test_gtest.cc
+++ test/test_gtest.cc
@@ -40,8 +40,3 @@ TEST(GoogleTestFrameworkTests, BooleanTest) {
     EXPECT_FALSE(false);
     EXPECT_FALSE(0);
 }
-
-int main(int argc, char* argv[]) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}

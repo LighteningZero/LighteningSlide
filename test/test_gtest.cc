862457cf3700a19b3c10678d808514a235bf65f5
szdytom <szdytom@163.com>
2020-08-19 09:00:48 +0800

    format code

--- test/test_gtest.cc
+++ test/test_gtest.cc
@@ -15,8 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include <gtest/gtest.h>
 #include <string>
+#include <gtest/gtest.h>
 
 TEST(GoogleTestFrameworkTests, EqualTest) {
     EXPECT_EQ(3, 1 + 2);
--- test/test_js.cc
+++ test/test_js.cc
@@ -15,9 +15,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#include <string>
 #include <gtest/gtest.h>
 
-#include <string>
 #include "js-runner/js.h"
 
 TEST(JSRunnerEngineTest, RunCodeTest) {

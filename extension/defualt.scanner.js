21fa2657a29d43a7b8ae4289cb45786fb65ecbea
szdytom <szdytom@163.com>
2020-08-20 09:58:47 +0800

    setLineBreakToLFMode

--- extension/defualt.scanner.js
+++ extension/defualt.scanner.js
@@ -50,6 +50,11 @@ function Scanner(text) {
         this.mark = undefined;
     }
 
+    this.setLineBreakToLFMode = () => {
+        this.text.replaceAll("\r\n", "\n");
+        this.text.replaceAll("\r", "\n");
+    }
+
     this.skipSpace = () => {
         let resualt = false;
         while (this.text[this.pointer] == ' ') {
